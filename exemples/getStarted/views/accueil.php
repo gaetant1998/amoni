5bf29189417131c42ec5f4e7f7b643d733e1f928
Gomsu gaëtant <gomsugaetant24@gmail.com>
2021-04-21 20:24:27 +0200

    ok

--- exemples/getStarted/views/accueil.php
+++ exemples/getStarted/views/accueil.php
@@ -10,14 +10,86 @@
 <body>
     
 <div class="main-container">
-    <div class="second-container txt-c">
-        <span style="position:relative;top:15px;background-color:white" class="ft-s-5 ft-f-6 p-5">
+    <div class="second-container txt-c h-20 ">
+        <span id='btn' style="position:relative;top:15px;background-color:white" class="ft-s-5 ft-f-6 p-5">
             hello world!
         </span>
+
         <hr>
+        
+        <div style="width: 500px; height:300px; border: 1px dashed black;margin:100px" id='drop-zone'>
+
+        </div>
+
         <div class="m-t-5 p-5"><a href="index.php?url=contact">contact</a></div>
+
+
     </div>
 </div>
-    
+    <script>
+        let btn = document.querySelector('#btn')
+        let  dropZone = document.querySelector('#drop-zone');
+        
+        let isMouseInElt = (x, y , elt) => {
+
+            if(x < elt.offsetLeft || x > (elt.offsetLeft + elt.offsetWidth)) {
+                return false
+
+            }
+            else if(y < elt.offsetTop || y > (elt.offsetTop + elt.offsetHeight)) {
+                return false;
+            }
+            
+            return true;
+        }
+
+        window.addEventListener('click', e => {
+            if(!isMouseInElt(e.clientX, e.clientY, btn)){
+                btn.style.color = 'red';
+            } else {
+                btn.style.color = 'black';
+            }
+        })
+
+        dropZone.addEventListener('dragenter', e=>{
+            e.target.style.borderColor = 'red';
+            console.log(e.target);
+        })
+
+        dropZone.addEventListener('dragover', e=>{
+            e.target.style.borderColor = 'red';
+            console.log(e.target);
+        })
+
+        dropZone.addEventListener('dragexit', e=>{
+            e.target.style.borderColor = 'red';
+            console.log(e.target);
+        })
+
+        dropZone.addEventListener('dragleave', e=>{
+            
+            // console.log(e);
+
+        })
+
+        /*
+        ondrag: null
+​
+ondragend: null
+​
+ondragenter: null
+​
+ondragexit: null
+​
+ondragleave: null
+​
+ondragover: null
+​
+ondragstart: null
+​
+ondrop: n
+        */
+
+    </script>
 </body>
 </html>
